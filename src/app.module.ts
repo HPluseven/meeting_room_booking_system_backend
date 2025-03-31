fd402234933469f5d13b1102f358437ddbe229b6
seven <seven.hjq@qq.com>
2025-04-01 01:50:36 +0800

    feat: updaye

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,7 +54,7 @@ import * as path from 'path';
           username: configService.get('mysql_server_username'),
           password: configService.get('mysql_server_password'),
           database: configService.get('mysql_server_database'),
-          // synchronize: true,
+          synchronize: true,
           logging: true,
           entities: [User, Role, Permission, MeetingRoom, Booking],
           poolSize: 10,
